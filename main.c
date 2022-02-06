82547ae3384b82cf0e29f051034c729b4cc25d37
XinShuo Wang <xinshuowang@163.com>
2022-02-06 08:08:06 +0800

    add dfs function

--- main.c
+++ main.c
@@ -11,6 +11,56 @@ extern void yy_delete_buffer(YY_BUFFER_STATE buffer);
 
 struct Value *val;
 
+void dfs(struct Value *v)
+{
+	//printf("v is %p\n", (void*)v);
+	if(v == NULL) return;
+	switch (v->type_)
+	{
+	case AND:
+		printf("AND \n");
+		break;
+	case OR:
+		printf("OR \n");
+		break;
+	case LESS:
+		printf("LESS \n");
+		break;
+	case LESS_OR_EQUAL:
+		printf("LESS_OR_EQUAL \n");
+		break;
+	case EQUAL:
+		printf("EQUAL \n");
+		break;
+	case NOT_EQUAL:
+		printf("NOT_EQUAL \n");
+		break;
+	case GREATER:
+		printf("GREATER \n");
+		break;
+	case GREATER_OR_EQUAL:
+		printf("GREATER_OR_EQUAL \n");
+		break;
+	case TO_YEAR:
+		printf("TO_YEAR \n");
+		break;
+	case STR_PARAM:
+		printf("STR_PARAM %s\n", (char *)v->data_);
+		break;
+	case INT_PARAM:
+		printf("INT_PARAM %d\n", *((int *)v->data_));
+		break;
+	case FLOAT_PARAM:
+		printf("FLOAT_PARAM %f\n", *((float *)v->data_));
+		break;
+	}
+	if(v->child_num_ == 0) return;
+	for(int i = 0; i < v->child_num_; ++i)
+	{
+		dfs(v->child_[i]);
+	}
+}
+
 int main()
 {
 	char string[] = "and(equals(toYear(LO_ORDERDATE), 1993), and(greaterOrEquals(LO_DISCOUNT, 1), "
@@ -18,6 +68,8 @@ int main()
 	YY_BUFFER_STATE buffer = yy_scan_string(string);
 	yyparse();
 	yy_delete_buffer(buffer);
-	printf("Value type is %d, op_size is %d, op is %s \n", val->type_, val->op_size_, (char*)val->op_);
+	printf("-----------------------\n");
+	printf("%s\n", string);
+	dfs(val);
 	return 0;
 }
